61619126f45e4029cd167996f58575a1216a57fa
alexp <alex.pilyavskiy03@gmail.com>
2025-10-27 19:41:36 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="container text-base">
       <div>Hello world!</div>
+      <div>Hello world!</div>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cumque
         doloribus maiores non provident saepe, sed! A asperiores consectetur
